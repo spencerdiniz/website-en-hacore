96d9a8fba82031ccb31d1370152a187a598b7095
Spencer Müller Diniz <spencerdiniz@gmail.com>
2025-12-04 12:58:59 -0300

    Atualização de informações do site. Inclusão do Instagram no header.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Projeto En-Hacoré";
+export const SITE_DESCRIPTION = "Transformando vidas com segurança, prevenção e cuidado na comunidade Novo Aarão Reis.";
